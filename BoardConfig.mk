b0a70a9c997336eb98d8db03de02cd01439e8a8a
Zvikomborero VIncent Zvikaramba <zvikovincent@gmail.com>
2016-08-13 00:43:49 -0400

    Updated file sepolicies.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -158,8 +158,6 @@ BOARD_PROVIDES_LIBRIL := false
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
-include vendor/cm/sepolicy/sepolicy.mk
-include vendor/cm/sepolicy/qcom/sepolicy.mk
 
 BOARD_SEPOLICY_DIRS += \
     device/samsung/gprimeltetmo/sepolicy
