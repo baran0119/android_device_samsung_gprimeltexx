d8951b2c9b42e48d50f04705825f32789466aea4
baran0119 <baran0119@gmail.com>
2016-10-15 12:13:48 +0200

    push all
    Change-Id: Id3ae6bed2bab4a83d74bd67b92e50316e6a2a56c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,14 +7,14 @@ LOCAL_PATH := device/samsung/gprimeltexx
 TARGET_OTA_ASSERT_DEVICE := gprimeltexx,grandprimelte,samsung_sm_g530fz,g530fz
 
 # Bootloader
-TARGET_BOOTLOADER_BOARD_NAME := msm8916
+#TARGET_BOOTLOADER_BOARD_NAME := msm8916
 
 # Init
 TARGET_LIBINIT_DEFINES_FILE := $(LOCAL_PATH)/init/init_gprimeltexx.c
 
 # Kernel
 BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/mkbootimg.mk
-TARGET_KERNEL_SOURCE := kernel/samsung/gprimeltexx
+# TARGET_KERNEL_SOURCE := kernel/samsung/gprimeltexx
 TARGET_KERNEL_VARIANT_CONFIG := msm8916_sec_fortuna_eur_defconfig
 
 # Misc.
@@ -23,3 +23,9 @@ TARGET_SYSTEM_PROP := $(LOCAL_PATH)/system.prop
 # Partition sizes
 BOARD_SYSTEMIMAGE_PARTITION_SIZE    := 1568669696
 BOARD_USERDATAIMAGE_PARTITION_SIZE  := 5637124096
+
+# TWRP
+ifneq ($(TW),)
+	RECOVERY_VARIANT := twrp
+	TARGET_CPU_VARIANT := cortex-a7
+endif
