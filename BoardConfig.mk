5b716b6ef9d90489b79962abbd7696b6391fa767
baran0119 <baran0119@gmail.com>
2016-10-15 12:16:59 +0200

    correct, clean up
    Change-Id: I4b92760313afedb07a6fbeb269b2ed2f207e7360

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,15 +6,11 @@ LOCAL_PATH := device/samsung/gprimeltexx
 # Assert
 TARGET_OTA_ASSERT_DEVICE := gprimeltexx,grandprimelte,samsung_sm_g530fz,g530fz
 
-# Bootloader
-#TARGET_BOOTLOADER_BOARD_NAME := msm8916
-
 # Init
 TARGET_LIBINIT_DEFINES_FILE := $(LOCAL_PATH)/init/init_gprimeltexx.c
 
 # Kernel
 BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/mkbootimg.mk
-# TARGET_KERNEL_SOURCE := kernel/samsung/gprimeltexx
 TARGET_KERNEL_VARIANT_CONFIG := msm8916_sec_fortuna_eur_defconfig
 
 # Misc.
