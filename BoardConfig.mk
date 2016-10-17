28d798c16da9c8065228a830f1a4780c8e0e6ddc
baran0119 <baran0119@gmail.com>
2016-10-17 20:53:46 +0200

    BoardConfig.mk: use my kernel sources
    Change-Id: I42bd6434c87b79b1a6d9afe293f1326044e80ff7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -11,6 +11,7 @@ TARGET_LIBINIT_DEFINES_FILE := $(LOCAL_PATH)/init/init_gprimeltexx.c
 
 # Kernel
 BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/mkbootimg.mk
+TARGET_KERNEL_SOURCE := kernel/samsung/gprimeltexx
 TARGET_KERNEL_VARIANT_CONFIG := msm8916_sec_fortuna_eur_defconfig
 
 # Misc.
