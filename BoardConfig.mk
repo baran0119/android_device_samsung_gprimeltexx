ed090ab3521b518c356a9c88f830b0ccebc2ca22
baran0119 <baran0119@gmail.com>
2016-10-20 00:53:44 +0200

    Set one specific defconfig
    Change-Id: Id04e9c1886a5e9a2d2c98c8fea748f211afa2103

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,7 +12,7 @@ TARGET_LIBINIT_DEFINES_FILE := $(LOCAL_PATH)/init/init_gprimeltexx.c
 # Kernel
 BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/mkbootimg.mk
 TARGET_KERNEL_SOURCE := kernel/samsung/gprimeltexx
-TARGET_KERNEL_VARIANT_CONFIG := msm8916_sec_fortuna_eur_defconfig
+TARGET_KERNEL_CONFIG := cyanogen_gprimeltexx_defconfig_defconfig
 
 # Misc.
 TARGET_SYSTEM_PROP := $(LOCAL_PATH)/system.prop
