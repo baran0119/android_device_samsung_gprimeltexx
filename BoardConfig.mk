d4cb0507e56e7aa1542cca4072e5ef308e227243
baran0119 <baran0119@gmail.com>
2016-10-20 15:05:55 +0200

    Correct defconfig name
    Change-Id: Ie3e71522b30ac485304527dae06f9bd08f455a70

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,7 +12,7 @@ TARGET_LIBINIT_DEFINES_FILE := $(LOCAL_PATH)/init/init_gprimeltexx.c
 # Kernel
 BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/mkbootimg.mk
 TARGET_KERNEL_SOURCE := kernel/samsung/gprimeltexx
-TARGET_KERNEL_CONFIG := cyanogen_gprimeltexx_defconfig_defconfig
+TARGET_KERNEL_CONFIG := cyanogen_gprimeltexx_defconfig
 
 # Misc.
 TARGET_SYSTEM_PROP := $(LOCAL_PATH)/system.prop
